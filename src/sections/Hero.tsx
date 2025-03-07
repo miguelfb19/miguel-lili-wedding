bea8c51486aaedfedc3edfc5345813e254dda705
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-03-07 14:56:06 -0500

    remove hero logo

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero = () => {
       <h1 className="text-center text-6xl md:text-8xl font-great-vibes text-nyanza-1">
         Miguel & Lili
       </h1>
-      <img src="/logo-wedding-white.png" alt="logo de la boda de Miguel y Lili" className="w-52"/>
+
       <span className="w-52 border-2 text-nyanza-1 font-montserrat py-5 mt-5 flex justify-center items-center font-bold rounded-tl-4xl rounded-br-4xl">
         02 · NOV · 2025
       </span>
