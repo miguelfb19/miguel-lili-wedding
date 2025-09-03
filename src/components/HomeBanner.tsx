74c525a97b9011ef3382040c18b2675b882364cc
Miguel Angel Fernandez <miguelangelfb19@utp.edu.co>
2025-09-03 09:21:31 -0500

    fix: update image paths in HomeBanner component and HTML meta tags

--- src/components/HomeBanner.tsx
+++ src/components/HomeBanner.tsx
@@ -4,7 +4,7 @@ import { useViewsStore } from "../store/views-store";
 export const HomeBanner = () => {
   const { setView } = useViewsStore();
   return (
-    <div className="h-screen overflow-hidden fade-in relative bg-nyanza-1 bg-[url('/sachet_sm.avif')] xl:bg-[url('/sachet_xl.avif')] bg-no-repeat bg-cover bg-center">
+    <div className="h-screen overflow-hidden fade-in relative bg-nyanza-1 bg-[url('/pictures/sachet_sm.avif')] xl:bg-[url('/pictures/sachet_xl.avif')] bg-no-repeat bg-cover bg-center">
       <div className="text-nyanza-1 text-center m-auto max-md:mx-3 md:w-3/5 pt-12 xl:pt-32 flex flex-col gap-2 xl:gap-8">
         <h1 className="font-great-vibes text-5xl md:text-6xl lg:text-7xl">
           Invitación a nuestra boda
@@ -20,7 +20,7 @@ export const HomeBanner = () => {
         <ArrowBigDown fill="currentColor" />
       </div>
       <button
-        className="absolute top-[45%] xl:top-[75%] 2xl:top-[80%] right-1/2 translate-x-[55%] z-30 bg-[url('/stamp.avif')] bg-cover bg-center h-32 w-32 md:h-44 md:w-44 animate-palpite cursor-pointer"
+        className="absolute top-[45%] xl:top-[75%] 2xl:top-[80%] right-1/2 translate-x-[55%] z-30 bg-[url('/pictures/stamp.avif')] bg-cover bg-center h-32 w-32 md:h-44 md:w-44 animate-palpite cursor-pointer"
         onClick={() => setView("main")}
       ></button>
     </div>
